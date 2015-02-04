31e90ad2b77e6dd906ded1a6596b9767dd3c1543
Gabriel Suarez <lixez@live.com.ar>
2015-02-04 18:33:25 -0300

    Probando JS externos

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    }
+
+
+    function onDeviceReady() {
+        
+	navigator.notification.alert("VIVMo 1.0 - Aplicación de Medición de Viveros by Moebius-IT. Le recordamos mantener su aplicación actualizada para mejor funcionamiento.","accion", "Bienvenido a VIVMo", "Continuar");
+}
